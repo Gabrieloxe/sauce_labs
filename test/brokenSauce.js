1145da9aa63e1deabf01667bc85c4283f70f4577
Gabriel Ong <gabrielongxe@gmail.com>
2022-10-02 23:52:34 +0800

    code after test time over

--- test/brokenSauce.js
+++ test/brokenSauce.js
@@ -1,5 +1,6 @@
-const {Builder, By, Key, until} = require('selenium-webdriver')
-const utils = require('./utils')
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const utils = require('./utils');
+const assert = require('assert');
 
 const SAUCE_USERNAME = process.env.SAUCE_USERNAME;
 const SAUCE_ACCESS_KEY = process.env.SAUCE_ACCESS_KEY;
@@ -7,50 +8,56 @@ const ONDEMAND_URL = `https://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.us-
 // NOTE: Use the URL below if using our EU datacenter (e.g. logged in to app.eu-central-1.saucelabs.com)
 // const ONDEMAND_URL = `https://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.eu-central-1.saucelabs.com:443/wd/hub`;
 
-
 /**
-* Run this test before working on the problem.
-* When you view the results on your dashboard, you'll see that the test "Failed".
-* Your job is to figure out why the test failed and make the changes necessary to make the test pass.
-*
-* Bonus: Once you get the test working, update the code so that when the test runs, it 
-* can reach the Sauce Labs homepage 
-* hover over 'Resources' and then clicks the 'Documentation' link
-*/
+ * Run this test before working on the problem.
+ * When you view the results on your dashboard, you'll see that the test "Failed".
+ * Your job is to figure out why the test failed and make the changes necessary to make the test pass.
+ *
+ * Bonus: Once you get the test working, update the code so that when the test runs, it
+ * can reach the Sauce Labs homepage
+ * hover over 'Resources' and then clicks the 'Documentation' link
+ */
 
 describe('Broken Sauce', function () {
-    it('should go to Google and click Sauce', async function () {
+  it('should go to Google and click Sauce', async function () {
+    try {
+      let driver = await new Builder()
+        .withCapabilities(utils.brokenCapabilities)
+        .usingServer(ONDEMAND_URL)
+        .build();
 
-        try {
-            let driver = await new Builder().withCapabilities(utils.brokenCapabilities)
-                    .usingServer(ONDEMAND_URL).build();
+      await driver.get('https://www.google.com');
+      // If you see a German or English GDPR modal on google.com you
+      // will have to code around that or use the us-west-1 datacenter.
+      // You can investigate the modal elements using a Live Test(https://app.saucelabs.com/live/web-testing)
 
-        await driver.get("https://www.google.com");
-        // If you see a German or English GDPR modal on google.com you 
-        // will have to code around that or use the us-west-1 datacenter.
-        // You can investigate the modal elements using a Live Test(https://app.saucelabs.com/live/web-testing)
+      let search = await driver.findElement(By.name('q'));
+      await search.sendKeys('Sauce Labs');
 
+      let button = await driver.findElement(By.name('btnK'));
+      await button.click();
 
-        let search = await driver.findElement(By.name("q"));
-        await search.sendKeys("Sauce Labs");
+      let page = await driver.findElement(By.partialLinkText('sauce'));
+      await page.click();
 
-        let button = await driver.findElement(By.name("btnK"))
-        await button.click();
+      let resources = await driver.findElement(By.linkText('Resources'));
+      const actions = await driver.actions({bridge: true});
+      await actions.move({origin: resources}).perform();
+      let documentation = await driver.findElement(
+        By.linkText('Documentation')
+      );
+      await documentation.click();
+      await driver.quit();
 
-        let page = await driver.findElement(By.partialLinkText("sauce"));
-        await page.click();
-
-        await driver.quit();
-        } catch (err) {
-            // hack to make this pass for Gitlab CI
-            // candidates can ignore this
-            if (process.env.GITLAB_CI === 'true') {
-                console.warn("Gitlab run detected.");
-                console.warn("Skipping error in brokenSauce.js");
-            } else {
-                throw err;
-            }
-        }
-
-    });
+    } catch (err) {
+      // hack to make this pass for Gitlab CI
+      // candidates can ignore this
+      if (process.env.GITLAB_CI === 'true') {
+        console.warn('Gitlab run detected.');
+        console.warn('Skipping error in brokenSauce.js');
+      } else {
+        throw err;
+      }
+    }
+  });
 });
